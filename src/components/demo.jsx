427aba528d4b47f2f803d3051906c00c6c75a566
unknown <jixuni@gmail.com>
2021-01-02 21:58:23 -0500

    added some comment

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -2,8 +2,19 @@ import React, { useState, useEffect } from "react";
 import { DataGrid } from "@material-ui/data-grid";
 
 export default function Demo() {
+  // using functional component instead of a class component
+  // it makes things a little more elegant without a large overhead isntead if you have a very complex react state
+  //Here im using react hook useState to initialize my state as an empty array.
+
+  // using hooks you initialize the state with useState - here im initialize an empty array
+  // useState needs 2 variables to be declare
+  //apiData is the "count/value of the current state"
+  //setApiData is the "counter/function" use to update the state - think of it as setState
   const [apiData, setApiData] = useState([]);
 
+  //useEffect think of it as a lifecycle method for class component, instead of using component did mount
+  //useEffect takes a callback of an empty array so this so the fetch() is only render 1 time after the initial render
+  // you can pass a variable like state/variable, this will make it call useeffect again when the value change e.g [props.value]
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/comments", {
       method: "GET",
